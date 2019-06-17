c279ffbdd2c3515c253ce25e2b6a087299bf0040
ErikBoesen <me@erikboesen.com>
2019-06-17 19:50:56 -0400

    feat: finish Location model __init__

--- yaledining/models.py
+++ yaledining/models.py
@@ -1,18 +1,10 @@
-class _base_model(dict):
+class _base_model():
     def __init__(self, raw: dict, api):
-        self.update(json)
-        self.update(self.__dict__)
-        self.__dict__ = self
+        self.raw = raw
         self.api = api
 
     def __repr__(self):
-        return '%s(%s)' % (self.__class__.__name__, self.json())
-
-    def json(self):
-        return dict.__repr__(self)
-
-    def raw(self):
-        return dict(self)
+        return '%s(%s)' % (self.__class__.__name__, self.raw)
 
 
 def _make_model(class_name):
@@ -31,21 +23,27 @@ class Manager:
 
 
 class Location(_base_model):
-    @property
-    def managers(self):
-        """
-        Get a list of managers in an easily manipulable format.
-
-        :return: list of Manager objects.
-        """
+    def __init__(self, raw: dict, api):
+        super().__init__(raw, api)
+        self.id = int(raw['ID_LOCATION'])
+        self.location_code = int(raw['LOCATIONCODE'])
+        self.name = raw['DININGLOCATIONNAME']
+        # TODO: enum?
+        self.type = raw['TYPE']
+        self.capacity = raw['CAPACITY']
+        self.percent_capacity = 10 * self.capacity if self.capacity is not None else None
+        self.geolocation = raw['GEOLOCATION']
+        self.latitude, self.longitude = raw['GEOLOCATION'].split(',')
+        self.closed = bool(raw['ISCLOSED'])
+        self.open = not self.closed
+        self.address = raw['ADDRESS']
+        self.phone = raw['PHONE']
         managers = []
         num_managers = 0
         while num_managers < 4:
             num_managers += 1
-
-            manager = (location[f'MANAGER{num_managers}NAME'],
-                       location[f'MANAGER{num_managers}EMAIL'])
-            if manager == (None, None):
-                break
-            managers.append(manager)
-        return managers
+            name = raw[f'MANAGER{num_managers}NAME']
+            email = raw[f'MANAGER{num_managers}EMAIL']
+            if name is not None and email is not None:
+                managers.append(Manager(name, email))
+        self.managers = tuple(managers)
