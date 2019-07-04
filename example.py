e2c591a1e371e4c7668f3bbf6c4030b922be488d
ErikBoesen <me@erikboesen.com>
2019-07-03 20:12:40 -0400

    Demo ingredients

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ api = yaledining.YaleDining()
 # Test parameters obtained from API documentation.
 locations = api.locations()
 print('There are %d locations, the name of one of them is %s' % (len(locations), locations[5].name))
-location = api.location('Hopper')
+location = api.location('Grace Hopper')
 print('%s is located at %s (latitude %f) and its phone number is %s.' % (location.name,
                                                                          location.address,
                                                                          location.latitude,
@@ -19,6 +19,8 @@ for meal in meals:
     print(meal.name + ':')
     for item in meal.items:
         print('%s %s vegetarian' % (item.name, 'is' if item.traits.vegetarian else 'isn\'t'))
+        ingredients = item.ingredients
+        print('Has %d ingredients, the most significant of which is %s' % (len(ingredients), ingredients[0]))
 
 # Or you can pass a menu item ID directly
 print(api.traits(5908402))
